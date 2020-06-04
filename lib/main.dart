93e5eef20ccb6b4a3fb1bdb15da2410779f33610
Olivia Lai <olivialai886@gmail.com>
2020-06-04 00:06:13 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,20 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('CHART!'),
+              color: Colors.blue,
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          ),
+        ],
       ),
     );
   }
